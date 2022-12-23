a4886c41f0ba4e9cd9ecbd1eb6ae91352e842fb2
shimu1806 <66287289+shimu1806@users.noreply.github.com>
2022-12-23 10:01:26 -0300

    Deixando mais apresentável, apenas
    eu disse apresentável em questão de estética, não cordialidade

--- Bin_to_Dec.py
+++ Bin_to_Dec.py
@@ -9,6 +9,7 @@ o valor 1, e assim segue. após ter o resultado de todas as bases, é realizado 
 no valor decimal 
 
 """
+print("CONVERSOR ATUALIZADO 2023 PARA CONVERTER NUMERO BINARIO EM DECIMAL, HELL YEAH\n\n")
 
 binario = int(input("Digita a porra do numerio binario, verme: ")) 
 n = len(str(binario)) # servirá para reduzir o comprimento do número binario, podendo realizar a operação de todas as posições
@@ -17,8 +18,8 @@ decimal = 0 # como o decimal começa a partir do 0, 0
 i = 0 # a base começa sendo elevada a 0, portanto, a variavel i, que representa o espoente é 0 
 
 while n >= 0: # o ciclo encerra quando o binario for totalmente operado
-        resto = binario % 10 
-        print("o resto deu: ", resto)
+        resto = binario % 10 # sabendo que o resultado sempre será 1 ou 0, podemos utilizar o resto ao invés de montar o cód em vetor
+        print("\no resto deu: ", resto)
         decimal = decimal + (resto * (2**i)) # o decimal vai aumentando conforme o programa operta, no caso, ele está recebendo o valor da base 2** ao esponte que cresce conforme o ciclo gira + o resto da divisão do número binário
         print("o decimal ta indo em: ", decimal)
         n = n - 1 # o comprimento vai reduzindo conforme segue o ciclo
@@ -28,4 +29,4 @@ while n >= 0: # o ciclo encerra quando o binario for totalmente operado
         binario = binario // 10 # binario vai reduzindo conforme segue o ciclo
         print("o binario ta indo em: ",binario)
         
-print("o binario que tu digitou deu: {}\ne ele converta para {} em decimal".format(valorDigitado,decimal))
+print("\n\no binario que que você digitou foi: {}\nque representa o valor: {} em decimal".format(valorDigitado,decimal))
